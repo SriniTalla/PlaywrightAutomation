978315bb1a15ea35150938d2ad33dd020d681fea
Srinivas Talla <tallasrinivas86@yahoo.com>
2025-07-17 21:06:29 +0100

    Added code

--- tests/Amazon.spec.js
+++ tests/Amazon.spec.js
@@ -1,6 +1,6 @@
 import { test, expect } from '@playwright/test';
 
-test('test', async ({ page }) => {
+test('testing', async ({ page }) => {
   await page.goto('https://www.amazon.co.uk/');
   await page.getByRole('link', { name: 'Best Sellers' }).click();
   await page.getByRole('button', { name: 'Accept' }).click();
@@ -8,4 +8,6 @@ test('test', async ({ page }) => {
   const element = await page.getByRole('link', { name: 'KITCHEN, You are currently ot' });
   await expect(element).toBeVisible();
   await page.close();
+
+
 });
